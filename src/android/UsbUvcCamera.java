3f9121746a2dc5bce29a7822e5377df72977cc8d
AleGar16 <alessio.garrone@anseltechnology.it>
2026-04-02 16:52:55 +0200

    fix import

--- src/android/UsbUvcCamera.java
+++ src/android/UsbUvcCamera.java
@@ -54,10 +54,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.LinkedHashSet;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class UsbUvcCamera extends CordovaPlugin {
